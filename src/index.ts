3d157af5f88054697734c45c8cd042521dfcfa24
Safarali Ramazonov <aliramazon@Safaralis-MacBook-Pro.local>
2021-03-25 23:23:26 -0600

    Add typescript to project

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-const express = require("express");
+import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 9000;
 
 app.get("/", (req, res) => res.send("GQL Backend 👍"));
 
-app.listen(PORT, () => console.log(`App is running on ${PORT} 👍`));
+app.listen(PORT, () => console.log(`Yahoo, App is running on ${PORT} 👍`));
