9a4835a739f82d954042b65044fb040290f4aaa9
Safarali Ramazonov <mr.safarali@gmail.com>
2021-04-04 22:45:31 -0600

    Add apollo-server-express and graphql

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import express from "express";
+import { listings } from "./listings";
 const app = express();
 const PORT = process.env.PORT || 9000;
 
+app.use(express.json({ limit: "10kb" }));
+
 app.get("/", (_req, res) => res.send("GQL Backend 👍"));
+
+app.get("/listings", (_req, res) => res.send(listings));
 
 app.listen(PORT, () => console.log(`App is running on ${PORT} 👍`));
